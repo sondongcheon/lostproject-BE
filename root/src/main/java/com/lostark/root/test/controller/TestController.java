af194fb4dfce98c4f5cf238d6e1e038d1423fc8e
손동천 <kij8025@naver.com>
2024-11-16 05:44:17 +0900

    test : testmapping 추가

---
+++ root/src/main/java/com/lostark/root/test/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.lostark.root.test.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/one")
+    public String testOne() {
+        return "testOne";
+    }
+}
