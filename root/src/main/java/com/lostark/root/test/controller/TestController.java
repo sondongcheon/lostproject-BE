0dd559e8adfd773a947ba073a68210219720674a
손동천 <kij8025@naver.com>
2024-12-11 01:39:15 +0900

    test56

--- root/src/main/java/com/lostark/root/test/controller/TestController.java
+++ root/src/main/java/com/lostark/root/test/controller/TestController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin("*")
 @RestController
 @RequestMapping("/test")
 public class TestController {
